99e56d2a8a1cce75b13b0c09319dc0b56c7e9093
Squiddy <wolaki96@gmail.com>
2022-10-11 16:03:13 -0700

    finished scheduler

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,48 @@
 //Adds the date for the beginning of the scheduler
 var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm a"));
+$("#currentDay").text(today.format("dddd, MMMM Do YYYY, hh:mm a"));
 
-//insert schedule template
 
-// 
+// for saving button
+
+$(saveBtn).on("click", function() {
+    console.log("hello");
+    var toDoToday = $(this).siblings(".description").val();
+    var timeOfDay = $(this).parent().attr("id");
+    localStorage.setItem(toDoToday, timeOfDay);
+})
+// add each hour to the local storage
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+//insert schedule template to change color based on time
+
+function workHour() {
+    var currentTime = moment().hour(); 
+    $(".time-block").each(function () {
+        var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+        console.log(currentTime, timeBlock);
+    
+    // past
+    if (currentTime > timeBlock ) {
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+    } else if (currentTime == timeBlock) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+    } else if (currentTime < timeBlock) {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    }
+})}
+workHour(); 
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Study_Scheduler
 A schedule to help plan out projects, homework, and studying.
+# <Work-Day-Scheduler>
+
+## Description
+I created this project to make it easy to track your schedule during the work day. One can now use this project to plan out their day while at work. This makes it easy to see when something has past, when something is coming up, what needs to be worked on in the present, and the capability of saving the schedule to your local storage. 
+
+In this project I learned how to create a local storage, as well as insert a time schedule from the moment API using JQuery.
+
+## Installation
+To install this project, simply download the file from github and open in VSCode or on your browser. 
+
+## Usage
+To use this product, type your schedule out on where the program tells you to type and the hour you desire to type your schedule. Then you can push the blue file button to save the information to your local storage. 
+
+
+
+    
+   ![alt text](assets/images/Screenshot%202022-10-11%20155703.png)
+   
+
+## Credits
+I had help from my classmates, Ian Binstock, Curran Duke, and my tutor Corey Yates on teaching me how to properly add buttons and removing/adding classes.
+
+
+
+
